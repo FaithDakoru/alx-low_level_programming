8c90e6577195ed9749725132fd3a6d3098994659
Faith Dakoru <Faithdakoru@yahoo.com>
2023-06-20 08:45:27 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (success)
- */
+ * main - Entry point
+ *
+ * Return: Always returns 0
+ *
+ */
 int main(void)
 {
 printf("_putchar\n");
