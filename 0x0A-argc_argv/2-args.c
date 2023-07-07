2d87490231bd6ce328ae5d2646dd79d5f39fe122
Faith Dakoru <Faithdakoru@yahoo.com>
2023-07-07 12:13:41 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
+
 /**
- * main - main function
+ * main - prints the number of arguments
+ * passed to the program
+ *
  * @argc: argumentc
+ *
  * @argv: vector of arguments
+ *
  *Return: always 0
  */
-int main(int argc, char  *argv[])
+int main(int argc, char *argv)
 {
+
 	int i;
 	for (i = 0; i < argc; i++)
 	{
